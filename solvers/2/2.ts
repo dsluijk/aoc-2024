edf3898c7faf3b593cb836c290e6a63b8de1d60f
Dany Sluijk <me@dany.dev>
2024-12-02 06:57:40 +0100

    feat: add day 2

---
+++ solvers/2/2.ts
@@ -0,0 +1,48 @@
+const isValid = (input: number[]): boolean => {
+  if (
+    !(
+      input.toSorted((a, b) => a - b).every((val, i) => val == input[i]) ||
+      input
+        .toSorted((a, b) => a - b)
+        .toReversed()
+        .every((val, i) => val == input[i])
+    )
+  )
+    return false;
+
+  if (
+    !input.every((val, i) =>
+      i > 0
+        ? Math.abs(val - input[i - 1]) <= 3 && Math.abs(val - input[i - 1]) > 0
+        : true
+    )
+  )
+    return false;
+
+  return true;
+};
+
+export const solve = (input: string) => {
+  const lines = input.split("\n");
+
+  let safeReports = 0;
+  for (const line of lines) {
+    const input = line.split(" ").map((el) => Number(el));
+
+    if (isValid(input)) {
+      safeReports += 1;
+      continue;
+    }
+
+    for (let i = 0; i < input.length; i++) {
+      const filteredInput = input.filter((_, ei) => i != ei);
+
+      if (isValid(filteredInput)) {
+        safeReports += 1;
+        break;
+      }
+    }
+  }
+
+  return safeReports;
+};

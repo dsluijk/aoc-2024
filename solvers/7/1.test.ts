7b569566495b702bb706555f115209866b5ab406
Dany Sluijk <me@dany.dev>
2024-12-07 06:23:36 +0100

    feat: add day 7

---
+++ solvers/7/1.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "bun:test";
+
+import { solve } from "./1";
+
+test("example", () => {
+  const example = `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`;
+
+  expect(solve(example)).toBe(3749);
+});
---
+++ solvers/7/2.ts
@@ -0,0 +1,31 @@
+const getTotals = (numbers: number[], searchValue: number): number[] => {
+  if (numbers.length == 0) return [];
+  if (numbers.length == 1) return numbers;
+
+  const rest = getTotals(numbers.slice(0, -1), searchValue);
+  return [
+    ...rest.map((v) => v + numbers[numbers.length - 1]),
+    ...rest.map((v) => v * numbers[numbers.length - 1]),
+    ...rest.map((v) =>
+      Number(v.toString() + numbers[numbers.length - 1].toString())
+    ),
+  ].filter((v) => v <= searchValue);
+};
+
+export const solve = (input: string) => {
+  const lines = input.split("\n");
+
+  let count = 0;
+  for (const line of lines) {
+    const numbers = line
+      .replace(":", "")
+      .split(" ")
+      .map((n) => Number(n));
+    const result = numbers[0];
+    const values = getTotals(numbers.slice(1), result);
+
+    if (values.filter((v) => v === result).length >= 1) count += result;
+  }
+
+  return count;
+};

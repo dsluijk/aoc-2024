a743e22c782096c3824f05757448394e3a946d50
Dany Sluijk <me@dany.dev>
2024-12-11 17:41:06 +0100

    feat(day11): add memoization

--- solvers/11/2.ts
+++ solvers/11/2.ts
@@ -3,21 +3,28 @@ export const solve = (input: string) => {
     .split(" ")
     .reduce((a, v) => ({ ...a, [v]: 1 }), {});
 
+  let nextValues: { [key: string]: string[] } = {};
   for (let i = 0; i < 75; i++) {
     let newStones: { [key: string]: number } = {};
 
     for (let [key, value] of Object.entries(stones)) {
       let nextKeys = [];
 
-      if (key === "0") {
-        nextKeys = ["1"];
-      } else if (key.length % 2 === 0) {
-        nextKeys = [
-          key.slice(0, key.length / 2),
-          key.slice(key.length / 2, key.length),
-        ];
+      if (key in nextValues) {
+        nextKeys = nextValues[key];
       } else {
-        nextKeys = [(Number(key) * 2024).toString()];
+        if (key === "0") {
+          nextKeys = ["1"];
+        } else if (key.length % 2 === 0) {
+          nextKeys = [
+            key.slice(0, key.length / 2),
+            key.slice(key.length / 2, key.length),
+          ];
+        } else {
+          nextKeys = [(Number(key) * 2024).toString()];
+        }
+
+        nextValues[key] = nextKeys;
       }
 
       for (const nextKey of nextKeys) {
